6dd42b186a0dba33c9c36729af7a4724c3d5d8db
Gabriel Ostrolucký <gabriel.ostrolucky@everlution.sk>
2017-09-25 23:33:57 +0200

    Message counter fix

--- tg-stalker.py
+++ tg-stalker.py
@@ -19,19 +19,21 @@ def stalk(receiver):
                 now = datetime.datetime.now()
                 date = colorama.Style.DIM + now.strftime("%d.%b (%a) %H:%M:%S") + colorama.Style.RESET_ALL
                 onlineStatus = (colorama.Fore.GREEN + 'online' if msg['online'] else colorama.Fore.RED + 'offline') + colorama.Fore.RESET
+                userId = msg['user']['id']
                 userName = msg['user']['print_name']
                 nickName = colorama.Fore.YELLOW + userName + colorama.Fore.RESET
                 lastChange = ''
-                if last is not {} and userName in last:
-                    diff = relativedelta(now, last[userName])
+                if last is not {} and userId in last:
+                    diff = relativedelta(now, last[userId])
                     lastChange = ' [%02d:%02d:%02d]' % (diff.hours, diff.minutes, diff.seconds)
                     if not msg['online']:
-                        lastChange = lastChange + ' (%d msgs)' % (msg_counter[userName] if userName in msg_counter else 0)
-                        msg_counter[userName] = 0
-                last[userName] = now
+                        lastChange = lastChange + ' (%d msgs)' % msg_counter.get(userId, 0)
+                        msg_counter[userId] = 0
+                last[userId] = now
                 print(date + ': ' + nickName + ': ' + onlineStatus + lastChange)
             elif msg['event'] == 'message':
-                msg_counter[msg['user']['print_name']] = msg_counter[userName] + 1 if userName in msg_counter[userName] else 1
+                userId = msg['sender']['id']
+                msg_counter[userId] = msg_counter.get(userId, 0) + 1
     except KeyboardInterrupt:
         receiver.stop()
         print("Exiting")
